6f49724582ca44975b6c19e4086e89c46f3beeb5
Masataka Okada <masataka.okada1117@gmail.com>
2022-11-12 18:00:52 +0900

    firebase導入

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
 // packages
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:firebase_core/firebase_core.dart';
 // models
 import 'package:udemy_flutter/main_model.dart';
+// options
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const ProviderScope(child: MyApp()));
 }
 
