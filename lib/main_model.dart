1f316d2dff518e81cfbd589d879551b90290bc59
Masataka Okada <masataka.okada1117@gmail.com>
2022-11-19 15:30:17 +0900

    Firestoreに接続

--- lib/main_model.dart
+++ lib/main_model.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 // packages
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final mainProvider = ChangeNotifierProvider(((ref) => MainModel()));
@@ -7,7 +8,17 @@ final mainProvider = ChangeNotifierProvider(((ref) => MainModel()));
 class MainModel extends ChangeNotifier {
   int counter = 0;
 
-  void incrementCounter() {
+  Future<void> createUser({required BuildContext context}) async {
+    final Map<String, dynamic> userData = {
+      'userName': 'Alice',
+      'uid': 'firstUser'
+    };
+    await FirebaseFirestore.instance
+        .collection('users')
+        .doc('firstUser')
+        .set(userData);
+    ScaffoldMessenger.of(context)
+        .showSnackBar(SnackBar(content: Text('ユーザーが作成できました')));
     counter++;
     notifyListeners();
   }
